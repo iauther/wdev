0d208a11438a48c8c6680c47b366f80a2c6b7c1c
gh <iauther@163.com>
2018-10-30 16:19:04 +0800

    add files

---
+++ web/js/device.js
@@ -0,0 +1,35 @@
+
+var Device=(function() {
+    var __proto=Device.prototype;
+    
+    __proto.i2c=function(speed) {
+        
+    }
+    
+    __proto.i2s=function(n,hl) {
+        
+    }
+    
+    __proto.spi=function(mode,speed) {
+        
+    }
+    
+    __proto.usb=function() {
+        
+    }
+    
+    __proto.gpio=function(n,hl) {
+        
+    }
+    
+    __proto.inet=function(url,port) {
+        
+    }
+})
+
+
+
+
+
+
+
---
+++ web/js/widget.js
@@ -0,0 +1,115 @@
+
+var Widget=(function() {
+    
+    this.Line=function() {
+        
+    }
+    
+    this.Rect=function() {
+        
+    }
+    
+    this.Circle=function() {
+        
+    }
+    
+    this.Ring=function() {
+        
+    }
+
+    this.Tips=function myListBox()
+    {
+        
+    }
+
+    this.Progress=function() {
+        
+    }
+
+    this.ListBox=function() {
+        
+    }
+    
+    this.Tree=function() {
+        
+    }
+    
+    this.Scroller=function() {
+        
+    }
+    
+    this.CheckBox=function() {
+        
+    }
+    
+    this.ComboBox=function() {
+        
+    }
+    
+    this.Menu=function() {
+        
+    }
+    
+    this.Table=function() {
+        
+    }
+
+    this.Button=function my()
+    {
+        
+    }
+
+    this.Select=function() {
+        
+    }
+
+    this.window=function myMultiSelect() {
+        
+    }
+
+    this.Slider=function () {
+        
+    }
+
+    this.Text=function() {
+        
+    }
+
+    this.Label=function() {
+        
+    }
+
+    this.TextArea=function() {
+        
+    }
+
+    this.Dialog=function() {
+        
+    }
+    
+    this.Window=function() {
+        
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
