f0ae0f264f2b0c922cb8a029bc3b520e98f43b9b
iauther <iauther@163.com>
2018-11-03 16:29:40 +0800

    完善

--- web/js/device.js
+++ web/js/device.js
@@ -2,6 +2,10 @@
 var Device=(function() {
     var _proto=Device.prototype;
     
+    _proto.bt=function() {
+        var __proto=_proto.i2c.prototype;
+    }
+    
     _proto.i2c=function(speed) {
         var __proto=_proto.i2c.prototype;
     }
@@ -20,20 +24,56 @@ var Device=(function() {
     
     _proto.gpio=function(n,hl) {
         var __proto=_proto.gpio.prototype;
+        
+        __proto.config=function(n,io) {
+            
+        }
+        
+        __proto.set_level=function(n,hl) {
+            
+        }
+        
+        __proto.get_level=function(n) {
+            
+        }
     }
     
-    _proto.inet=function(url,port) {
-        var __proto=_proto.inet.prototype;
+    _proto.uart=function(rate) {
+        var __proto=_proto.gpio.prototype;
         
-        __proto.connect=function() {
+        __proto.config=function(n,rate) {
             
         }
         
-        __proto.connect=function() {
+        __proto.write=function(n,da) {
             
         }
+        
+        __proto.write=function(n,rate) {
+            
+        }
+        
         
     }
+    
+    _proto.eth=function() {
+        var __proto=_proto.gpio.prototype;
+    }
+    
+    _proto.wifi=function(url,port) {
+        var __proto=_proto.inet.prototype;        
+        
+        __proto.connect=function() {
+            
+        }
+    }
+    
+    _proto.ws=function(url,port) {
+        var __proto=_proto.ws.prototype;
+        
+    
+    }
+    
 })()
 
 
