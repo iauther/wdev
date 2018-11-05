47de523ba41923a38facb39c5f8d190880af8214
gh <iauther@163.com>
2018-11-05 17:29:16 +0800

    add to html, ready to run

--- web/js/data.js
+++ web/js/data.js
@@ -1,3 +1,26 @@
+
+function header(data) {
+            var tmp, len, length=0;
+            
+            len = 4;
+            tmp = new DataView(data, length, len);
+            length += len;
+            this.evt = tmp.getInt32(0,true);
+            
+            len = 4;
+            tmp = new DataView(data, length, len);
+            length += len;
+            this.flag = tmp.getInt32(0,true);
+            
+            len = 4;
+            tmp = new DataView(data, length, len);
+            length += len;
+            this.dlen = tmp.getInt32(0,true);
+            
+            //this.data = ;
+        }
+
+
 var Data=(function() {
     var _proto=Data.prototype;
     
--- web/js/main.js
+++ web/js/main.js
@@ -1,41 +1,79 @@
 
 function Area(x,y,w,h)
 {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
+    this.x=x;
+    this.y=y;
+    this.w=w;
+    this.h=h;
 }
 
 
 
-function myTtl(area,str)
+function myTitle(area,txt,data)
 {
     
 }
 
 
-function myEq()
+function myMenu(area,txt,data)
 {
     
 }
 
 
-function myDyn()
+////////////subedit///////////////
+function myFader(area,txt,data)
+{
+    
+}
+function myEqualizer(area,txt,data)
+{
+    
+}
+function myDynamic(area,txt,data)
+{
+    
+}
+function myChannel(area,txt,data)
+{
+    var eq,dyn,fader;
+    
+    eq    = new myEq();
+    dyn   = new myDyn();
+    fader = new myFader();
+    
+}
+////////////////////////////////////
+function myEdit(area,txt,data)
 {
     
 }
 
 
-function myUpg()
+function mySetup(area,txt,data)
 {
     
 }
 
 
-function myFader()
+function myUpgrade(area,txt,data)
 {
     
 }
 
 
+///////////////////////////////////////////
+
+
+
+
+var scr=(function myScr() {
+    var i;
+    var st=Laya.stage;
+    
+    this.tt = new myTitle(st, Area(0,0,0,0), );
+    this.mn = new myMenu(stArea(0,0,0,0), );
+    this.et = new myEdit(stArea(0,0,0,0), );
+    this.ug = new myUpgrade(stArea(0,0,0,0), );
+    
+})()
